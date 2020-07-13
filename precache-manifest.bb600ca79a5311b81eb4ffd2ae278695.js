1260068ff34dadbf1ad559e7402ba84dddfeaa90
kakiz <nadaletriel@gmail.com>
2020-07-13 15:03:58 -0300

    Updates

---
+++ precache-manifest.bb600ca79a5311b81eb4ffd2ae278695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130e022b28cb179c897e955196f68a60",
+    "url": "/Sort-Visualizer/index.html"
+  },
+  {
+    "revision": "aa1e07a444a4ec09464b",
+    "url": "/Sort-Visualizer/static/css/main.fd0bbde2.chunk.css"
+  },
+  {
+    "revision": "a2a7d6b63bbdcc0a584e",
+    "url": "/Sort-Visualizer/static/js/2.f9e8c330.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Sort-Visualizer/static/js/2.f9e8c330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa1e07a444a4ec09464b",
+    "url": "/Sort-Visualizer/static/js/main.aa4b5b10.chunk.js"
+  },
+  {
+    "revision": "36fc6552b0bc95a4ce80",
+    "url": "/Sort-Visualizer/static/js/runtime-main.1d3e9fd4.js"
+  }
+]);
